fb0b9b79d812343338bc2d6ff44276ea6da9f8f4
Recipe Finder User <recipefinder@example.com>
2025-09-05 19:07:42 +0530

    Initial commit of recipe finder project

---
+++ recipe-finder-main/recipe-finder-main/src/pages/HomePage/HomePage.js
@@ -0,0 +1,38 @@
+// HomePage.js
+import React from 'react';
+import './HomePage.css';
+import { useNavigate } from "react-router-dom";
+import LinkedInLogo from '../../assets/images/LinkedInLogo.png';
+import initials from '../../assets/images/initials.png';
+
+
+
+const HomePage = () => {
+  let navigate = useNavigate(); 
+  const routeChange = () =>{ 
+    let path = `/search`; 
+    navigate(path);
+  }
+
+  return (
+    <div className="container">
+      <div className="content-main">
+        <h1 className="title">👨‍🍳 Recipe Finder 🍴</h1>
+        <p className="description">Easily discover recipes by simply entering your available ingredients.</p>
+        <button className="ctaButton" onClick={routeChange}>Try it Out</button>
+      </div>
+      <footer className="footer-main">
+        <div className="social-links">
+          <a href="www.linkedin.com/in/insha-quamar-419895258" target="_blank" rel="noopener noreferrer">
+            <img src={LinkedInLogo} alt="LinkedIn" />
+          </a>
+          <a href="https://github.com/InshaQuamar" target="_blank" rel="noopener noreferrer">
+            <img src={initials} alt="Portfolio" />
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default HomePage;
